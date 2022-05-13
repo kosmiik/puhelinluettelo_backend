448d200a33d5eef336178890ec20173015e3fb94
kosmiik <miika@koske.la>
2022-05-13 22:35:14 +0300

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
+
 
 app.use(morgan((tokens, req, res) => {
   return [tokens.method(req, res),
